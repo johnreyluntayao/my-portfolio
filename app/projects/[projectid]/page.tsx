404a2356e082aaa384f1a29cd5a836103181cdc9
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2024-08-28 14:24:15 +0800

    Added the project details section

--- app/projects/[projectid]/page.tsx
+++ app/projects/[projectid]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Footer from '@/components/Footer'
+import ProjectAbout from '@/components/ProjectAbout'
 import ProjectHero from '@/components/ProjectHero'
 import { FloatingNav } from '@/components/ui/FloatingNavbar'
 import { navItems } from '@/data'
@@ -15,10 +16,12 @@ const page = ({params} : {
 
   return (
     
-    <main>
+    <main className="relative bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
        <FloatingNav navItems={navItems}/>
        <ProjectHero id={id}/>
-
+       <ProjectAbout id={id}/>
+      </div>
     </main>
    
   )
