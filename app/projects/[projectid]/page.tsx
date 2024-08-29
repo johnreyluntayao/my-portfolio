4071d4df5ab48e5caf946b517a43b30b04edbc2d
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2024-08-29 17:36:16 +0800

    Added the project features section for each project

--- app/projects/[projectid]/page.tsx
+++ app/projects/[projectid]/page.tsx
@@ -2,6 +2,8 @@
 
 import Footer from '@/components/Footer'
 import ProjectAbout from '@/components/ProjectAbout'
+import ProjectContribution from '@/components/ProjectContribution'
+import ProjectFeatures from '@/components/ProjectFeatures'
 import ProjectHero from '@/components/ProjectHero'
 import { FloatingNav } from '@/components/ui/FloatingNavbar'
 import { navItems } from '@/data'
@@ -15,15 +17,21 @@ const page = ({params} : {
 
 
   return (
-    
+    <>
     <main className="relative bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
        <FloatingNav navItems={navItems}/>
        <ProjectHero id={id}/>
        <ProjectAbout id={id}/>
+       <ProjectContribution />
       </div>
+      
+    
     </main>
-   
+    <ProjectFeatures />
+    <Footer />
+    
+    </>
   )
 }
 
---
+++ components/ProjectContribution.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BoxHover } from './ui/BoxHover'
+
+const ProjectContribution = () => {
+  return (
+    <section>
+        <h1 className='heading pt-3 md:pt-5 lg:pt-10'>
+            My {' '} 
+            <span className='text-science-blue-600'>Contributions</span>
+        </h1>
+        <BoxHover />
+    </section>
+  )
+}
+
+export default ProjectContribution
