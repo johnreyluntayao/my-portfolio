cc2525a24665aa657534935c6ea2f381a413887c
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2024-09-07 16:40:17 +0800

    Change the design for my role section

--- app/projects/[projectid]/page.tsx
+++ app/projects/[projectid]/page.tsx
@@ -32,7 +32,7 @@ const page = ({params} : {
 
     <div className=" bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <ProjectContribution />  
+        <ProjectContribution id={id}/>  
     </div>
   
       <Footer />
