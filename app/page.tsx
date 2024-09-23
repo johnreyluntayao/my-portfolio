41fbb383f22122529d274adcd1013ba65f4ae034
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2024-09-23 18:09:29 +0800

    fixed some error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/About";
 import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
