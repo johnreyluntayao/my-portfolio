4e9d1aebf9197628eac38a79c05fe7e5da8fa43f
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-01-11 15:23:21 +0800

    refactor: glowing button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -93,7 +93,7 @@ const Hero = () => {
               title="Know more"
               icon={<HiArrowNarrowDown />}
               position='right'
-              moreClasses='text-white font-bold bg-[linear-gradient(110deg,#0d91ea,45%,#7cc7fd,55%,#0d91ea)]'
+              moreClasses='text-white bg-[linear-gradient(110deg,#0d91ea,45%,#7cc7fd,55%,#0d91ea)]'
               // bg-[linear-gradient(110deg,#fd7c76,45%,#ff908a,55%,#fd7c76)]
             />
           </motion.a>
